eeecc1cc5cc9f7d3ab3b0dbfd57ec0d709e2a675
fantasticiOS <422538722@qq.com>
2016-09-01 10:29:11 +0800

    Initial Commit

---
+++ EZLoadingView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EZLoadingView
+//
+//  Created by odm2 on 16/9/1.
+//  Copyright © 2016年 fantasticiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EZLoadingView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EZLoadingView
+//
+//  Created by odm2 on 16/9/1.
+//  Copyright © 2016年 fantasticiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
